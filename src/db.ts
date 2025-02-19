1717bf64f0ea4daa53e2b9d4851040f6b8767c3f
Andrii Klymiuk <ext-andrii.klymiuk@skeepers.io>
2025-02-19 12:25:28 +0200

    Added test user

--- src/db.ts
+++ src/db.ts
@@ -4,3 +4,13 @@ export const db: Database = {
   users: new Map<string, User>(),
   recipes: new Map<string, Recipe>(),
 };
+
+const testUser: User = {
+  id: "test-user-123",
+  name: "Test User",
+  email: "test@example.com",
+  createdAt: new Date(),
+  updatedAt: new Date()
+};
+
+db.users.set(testUser.id, testUser);
